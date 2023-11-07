7731be59ef1dde3b2c51e1560a1637e99ce46dee
PranavChaturvedi <PranavChaturvedi@users.noreply.github.com>
2023-11-07 21:56:35 +0530

    This is the answer to part 2. its very similar to part 1 all i did was just create another global variable called index and a list called indices then i just iterated through the data using the same for loop as part 1 and incrimented the value of the index no matter what the charracter was. i then used the append function to capture the values of the index where floor was -1. then out of all the elements of the list indices i just printed out the first element which would be the first time santa goes to the basement. and that was the answer to part 2.

--- 2015/day1/part2.py
+++ 2015/day1/part2.py
@@ -4,11 +4,16 @@ data = base_path.joinpath("input.in").read_text().strip()
 # print(data)
 
 floor = 0
+index = 0
+indices = []
 
 for brackets in data:
     if brackets == "(":
         floor += 1
+        index += 1
     elif brackets == ")":
         floor -= 1
-
-print("Answer to part 1 : ", floor)
+        index += 1
+    if floor == -1:
+        indices.append(index)
+print("Answer to part 2 : ", indices[0])
