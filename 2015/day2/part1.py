853409a453254872e7ee7368b0e920d3e3ba9321
PranavChaturvedi <PranavChaturvedi@users.noreply.github.com>
2023-11-08 19:00:54 +0530

    part 1 solved

---
+++ 2015/day2/part1.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+base_path = Path(__file__).parent
+data = base_path.joinpath("input.in").read_text().strip().splitlines()
+
+# print(data)
+
+surface_area = 0
+extra_area = 0
+
+for values in data:
+    lwh = values.split("x")
+    
+    # print(lwh)
+
+    length = int(lwh[0])
+    width = int(lwh[1])
+    height = int(lwh[2])
+
+    surface_area += 2 * length * width + 2 * width * height + 2 * height * length
+    
+    if length >= width and length >= height:
+        extra_area += width * height
+    elif width >= length and width >= height:
+        extra_area += length * height
+    else:
+        extra_area += length * width
+
+print(surface_area + extra_area)
